078c03864cf0f4a575996fa17b5c9ca633d9c85a
masiravaibhavi <53028923+masiravaibhavi@users.noreply.github.com>
2019-07-18 10:26:41 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,8 @@
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+c=float(input("enter third number:"))
+if(a>b)&(a>c):
+  largest=a
+elif(b>a)&(b>c):
+   largest=c
+print("the largest no is",largest)   
